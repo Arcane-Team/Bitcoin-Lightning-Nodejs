9784e8c72e49f1c7f3e5884d898788dbe591bb00
Oleg Lukashev <incognitorus@gmail.com>
2020-12-03 20:04:47 +0400

    test

--- auth.js
+++ auth.js
@@ -10,10 +10,10 @@ function define(obj, name, value) {
 exports.config = {};
 
 //define your channel host here e.g define(exports.config, "HOST", "localhost:10001");
-define(exports.config, "HOST", CHANNEL_HOST);
+define(exports.config, "HOST", "127.0.0.1:10009");
 
 //define your tls.cert file url e.g define(exports.config, "CERT"     , "/home/tarun/.lnd/tls.cert");
-define(exports.config, "CERT", CERT_FILE);
+define(exports.config, "CERT", "/Users/oleg/Library/Application\ Support/Lnd/tls.cert");
 
 //define your admin.macaroon file url here e.g define(exports.config, "AUTH"       , "/home/tarun/go/dev/alice/data/admin.macaroon");
-define(exports.config, "AUTH", AUTH_FILE);
+define(exports.config, "AUTH", "/Users/oleg/Library/Application\ Support/Lnd/data/chain/bitcoin/testnet/admin.macaroon");
